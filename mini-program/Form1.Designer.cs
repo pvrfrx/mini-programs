4bde9e0479f7625c13c57577a5bfb3b102440026
pvrfrx@gmail.com <javarush1>
2016-08-29 16:47:15 +0500

    initial

--- mini-program/Form1.Designer.cs
+++ mini-program/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace mini_program
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "MainForm";
+            this.Text = "Mini programs";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- mini-program/Program.cs
+++ mini-program/Program.cs
@@ -16,7 +16,7 @@ namespace mini_program
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
